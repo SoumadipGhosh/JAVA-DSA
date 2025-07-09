16fa413589889439762a5ee6a4bef295796d211c
soumadip <soumadipghosh01@gmail.com>
2025-07-09 23:11:13 +0530

    new

---
+++ ArraySort.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class ArraySort {
+    public static void main(String[] args) {
+        int[]arr={1,2,3,7,5};
+
+        // sort ..
+        Arrays.sort(arr);
+
+        for (int i= 0; i< arr.length;i++) {
+            System.out.println(arr[i]);
+            
+        }
+    }
+}
