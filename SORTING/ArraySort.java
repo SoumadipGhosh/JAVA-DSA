425ccab2906702cb8aefde35c22f70944813d86d
soumadip <soumadipghosh01@gmail.com>
2025-07-09 23:14:44 +0530

    new

--- SORTING/ArraySort.java
+++ SORTING/ArraySort.java
@@ -3,7 +3,7 @@ public class ArraySort {
     public static void main(String[] args) {
         int[]arr={1,2,3,7,5};
 
-        // sort ..
+        // sort by collection framework..
         Arrays.sort(arr);
 
         for (int i= 0; i< arr.length;i++) {
