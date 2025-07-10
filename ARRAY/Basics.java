1d8ba06e3ef7b8fa86d0795efaebbadff79f5cd5
soumadip <soumadipghosh01@gmail.com>
2025-07-10 14:50:05 +0530

    new

---
+++ ARRAY/Basics.java
@@ -0,0 +1,11 @@
+public class Basics {
+    public static void main(String[] args) {
+        // store 5 rollnos..
+        // int[]rollnos=new int[5];
+        //or
+        int[]rollnos={1,2,3,4,5};
+        // object is created in heap ..
+        System.out.println(rollnos[3]);
+
+    }
+}
