409c71478236f6e53181352d8980c25a0d79da91
soumadip <soumadipghosh01@gmail.com>
2025-07-19 15:46:52 +0530

    new

---
+++ Strings/Basics.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class Basics {
+    public static void main(String[] args) {
+        String name="Soumadip Ghosh";
+
+
+        String a="Soumadip";
+        System.out.println(a);
+        a="Ghosh"; // we dont change the object we create a new object ..
+        System.out.println(a);
+
+        // System.out.println(name);
+
+    }
+}
+ 
---
+++ Strings/New.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class New {
+    public static void main(String[] args) {
+        String a=new String("Kunal");
+        String b=new String("Kunal");
+
+    // System.out.println(a==b);
+    System.out.println(a.equals(b));
+
+
+    // give false .. look like pointin to the same object but not thats true ..pointing diff objs..
+
+}
+}
